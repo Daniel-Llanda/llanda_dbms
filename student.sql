e056c429bde2d31fe391a5afc033d7b91b6e2758
Daniel Llanda <111955991+Daniel-Llanda@users.noreply.github.com>
2023-05-19 02:48:12 -0700

    Add files via upload

---
+++ student.sql
@@ -0,0 +1,74 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: May 19, 2023 at 02:54 PM
+-- Server version: 10.4.24-MariaDB
+-- PHP Version: 7.4.29
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `student_info`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `student`
+--
+
+CREATE TABLE `student` (
+  `studno` int(11) NOT NULL,
+  `studFName` varchar(50) DEFAULT NULL,
+  `studLName` varchar(50) DEFAULT NULL,
+  `major` varchar(50) DEFAULT NULL,
+  `gender` varchar(50) DEFAULT NULL,
+  `homeaddress` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `student`
+--
+
+INSERT INTO `student` (`studno`, `studFName`, `studLName`, `major`, `gender`, `homeaddress`) VALUES
+(1, 'ANA', 'CRUZ', 'ECE', 'F', 'MALOLOS,BULACAN'),
+(2, 'HAZEL', 'PONCE', 'COE', 'F', 'PLARIDEL,BULACAN'),
+(3, 'CRISTOV', 'CONCIO', 'ECE', 'M', 'MALOLOS,BULACAN'),
+(4, 'ARIS', 'CLAVIO', 'COE', 'M', 'POLILAN,BULACAN'),
+(5, 'HONEY', 'PELAYO', 'COE', 'F', 'PLARIDEL,BULACAN'),
+(6, 'CARRIE', 'ABO', 'ECE', 'F', 'POLILAN,BULACAN');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `student`
+--
+ALTER TABLE `student`
+  ADD PRIMARY KEY (`studno`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `student`
+--
+ALTER TABLE `student`
+  MODIFY `studno` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
